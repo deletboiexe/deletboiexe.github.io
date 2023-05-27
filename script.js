8cffe3b5c3c06f8a5d228d5f620f346238284ce6
deletboiexe <69753212+deletboiexe@users.noreply.github.com>
2023-05-27 20:57:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+    var userAgent = navigator.userAgent.toLowerCase();
+  
+    if (/mobile|android/.test(userAgent)) {
+      var mobileMessage = document.getElementById("mobileMessage");
+      if (mobileMessage) {
+        mobileMessage.style.display = "block";
+      }
+    }
+  });
+  
