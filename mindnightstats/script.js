a1b47ff4f6171d3592c5682f9b20d97199230a85
deletboiexe <69753212+deletboiexe@users.noreply.github.com>
2024-06-06 14:48:33 +0200

    0.2b.11

--- mindnightstats/script.js
+++ mindnightstats/script.js
@@ -1,9 +1,8 @@
 const sheetId = '1kEfo-PIHyzszq2i9vtdHa33rNSIkuWolotTDJyF4wLg';
 const apiKey = 'AIzaSyCwzZeg9z4bnUQXQe6Bez7KrIVrOZQ3WGs';
-const range = 'Sheet1'
 
 async function fetchData() {
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
     displayData(data);
